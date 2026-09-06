488a51bc35ce537472c7c059b561688ff853e4ab
ibrahemMohsen <ibrahemmohsen851@gmail.com>
2026-09-06 12:41:12 +0300

    Question01

--- CSharpBasics04/Program.cs
+++ CSharpBasics04/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            //double[] prices = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(prices[1]);
+            #endregion
+
+
         }
     }
 }
